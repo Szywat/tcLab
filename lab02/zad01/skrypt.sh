dbbb1993c812e2ad28038f1e034d032d09e6088c
szywat <szywat04@gmail.com>
2025-06-02 20:50:21 +0200

    lab02

---
+++ lab02/zad01/skrypt.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+info() {
+    echo -e "\n\033[1;34m[$1]\033[0m $2"
+}
+
+NODE_VERSION="12"
+
+info "KONFIGURACJA" "Używam Node.js w wersji $NODE_VERSION"
+
+info "KONTENER" "Tworzę i uruchamiam kontener Docker z Node.js $NODE_VERSION"
+CONTAINER_ID=$(docker run -d -p 8080:8080 --name zadanie01-http-container -it node:$NODE_VERSION-alpine sh -c "while true; do sleep 3600; done")
+
+echo "Utworzono kontener o ID: $CONTAINER_ID"
+
+info "STRUKTURA" "Tworzenie katalogu /app w kontenerze"
+docker exec $CONTAINER_ID mkdir -p app
+
+info "KOPIOWANIE" "Kopiowanie plików aplikacji do kontenera za pomocą docker cp"
+docker cp package.json $CONTAINER_ID:/app/
+docker cp http.js $CONTAINER_ID:/app/
+
+info "ZALEŻNOŚCI" "Instalacja zależności Node.js wewnątrz kontenera"
+docker exec --workdir /app $CONTAINER_ID npm install
+
+info "URUCHOMIENIE" "Uruchamianie aplikacji Node.js w kontenerze"
+docker exec --workdir /app $CONTAINER_ID node http.js
+
+info "SPRZĄTANIE" "Aby zatrzymać i usunąć kontener, wykonaj:"
+echo "docker stop $CONTAINER_ID"
+echo "docker rm $CONTAINER_ID"
